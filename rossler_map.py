7eafecbfe00ae3778d08c73d7e6c1532b1790e5b
LouisDumont <dumont.louis21@gmail.com>
2020-03-12 15:40:23 +0100

    Add comments on rossler_map

--- rossler_map.py
+++ rossler_map.py
@@ -13,6 +13,9 @@ class RosslerMap:
         _.delta_t = delta_t
 
     def v_eq(_, t=None, v=None):
+        '''
+        From a vector of position v for a particule, returns its speed.
+        '''
         x, y, z = v[0], v[1], v[2]
         dot_x = -y - z
         dot_y = x + _.a*y
@@ -20,6 +23,9 @@ class RosslerMap:
         return np.array([dot_x, dot_y, dot_z])
 
     def jacobian(_, v):
+        '''
+        Returns J such that dot_X = J*X (* is matrix multiplication)
+        '''
         x, z = v[0], v[2]
         res = np.array([[       0,      -1,       -1],
                        [        1,     _.a,        0],
@@ -27,11 +33,17 @@ class RosslerMap:
         return res
 
     def full_traj(_, nb_steps, init_pos):
+        '''
+        Generates a trajectory from init_pos (?)
+        '''
         t = np.linspace(0, nb_steps * _.delta_t, nb_steps)
         f = solve_ivp(_.v_eq, [0, nb_steps * _.delta_t], init_pos, method='RK45', t_eval=t)
         return np.moveaxis(f.y, -1, 0),t
     
     def equilibrium(_):
+        '''
+        Returns the equilibrium position of the system (closed form)
+        '''
         x0 = (_.c-np.sqrt(_.c**2-4*_.a*_.b))/2
         y0 = (-_.c+np.sqrt(_.c**2-4*_.a*_.b))/(2*_.a)
         z0 = (_.c-np.sqrt(_.c**2-4*_.a*_.b))/(2*_.a)
