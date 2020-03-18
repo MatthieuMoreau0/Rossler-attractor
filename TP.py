Fix jacobian loss, rafine stats parameters

TP.py
@@ -19,7 +19,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from speed_NN import *
-from stats import *
 
 
 class dataset(torch.utils.data.Dataset):
@@ -186,8 +185,8 @@ if __name__ == '__main__':
     datasetTrain = dataset(x_train.astype("float32"),y_train.astype("float32"),s_train.astype("float32"),j_train.astype("float32"))
     datasetVal = dataset(x_val.astype("float32"),y_val.astype("float32"),s_val.astype("float32"),j_val.astype("float32"))
     #model = NN_model()
-    model = SpeedNN_model(lambda_jacob=0.01)
-    model.train(datasetTrain, batch_size=250, epochs=20, shuffle = True, test = datasetVal)
+    model = SpeedNN_model(lambda_jacob=0.1)
+    model.train(datasetTrain, batch_size=250, epochs=10, shuffle = True, test = datasetVal)
     model.save_weight()
 
 

speed_NN.py
@@ -72,9 +72,11 @@ class SpeedNN_model():
             self.model.train()
             for x,y,s,j in train_loader:
                 #print('x grad 1', x.grad)
+                x_copy = x.clone()
+                #print('x before backprop:', x)
                 if self.lambda_jacob is not None: x.requires_grad = True 
                 #print('x grad 2', x.grad)
-                self.optimizer.zero_grad()
+                #self.optimizer.zero_grad()
                 output, speed = self.model(x)
                 #print('output_shape:', output.shape)
                 loss_out = self.criterion1(output, y)
@@ -104,11 +106,14 @@ class SpeedNN_model():
                     loss += self.lambda_jacob * loss_jacob
                     x.requires_grad = False # remove grad from x to avoid modifying the input
                     list_loss_jac.append(loss_jacob.detach().numpy())
-                print('Computed jacobian:', jacobian)
-                print('Original jacobian:', j)
-
+                    #print('Computed jacobian:', jacobian)
+                    #print('Original jacobian:', j)
+                self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
+                # Check that the input was not modified when back_propagating
+                #print('x after backprop:', x)
+                #print('Diff:', np.unique(x_copy.detach().numpy()-x.detach().numpy()))
                 list_loss_pos.append(loss_out.detach().numpy())
                 list_loss_speed.append(loss_speed.detach().numpy())
                 list_loss.append(loss.detach().numpy())

stats.py
@@ -1,8 +1,10 @@
+import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from TP import *
 from rossler_map import *
 from dtw import *
+from time_series import *
 
 
 def draw_histogram(traj,y):
@@ -37,6 +39,7 @@ def joint_distrib(gt_traj, sim_traj, T):
   Plots the joint distributions of (w(t),w(t+T)) (for each axis) for w in {traj, y}
   '''
   labels = ["x","y","z"]
+  #print(len(gt_traj))
   for i in range(3):
     sim_coords = sim_traj[:-T,i]
     gt_coords = gt_traj[:-T,i]
@@ -46,14 +49,14 @@ def joint_distrib(gt_traj, sim_traj, T):
     fig=plt.figure()
 
     ax1 = fig.add_subplot(1,2,1)
-    ax1.hist2d(gt_coords, translat_gt_coords, bins=30)
+    ax1.hist2d(gt_coords, translat_gt_coords, bins=20)
     plt.ylabel(f"{labels[i]}(t+T)")
     plt.xlabel(f"{labels[i]}(t) (true system)")
 
     ax2 = fig.add_subplot(1,2,2)
-    ax2.hist2d(sim_coords, translat_sim_coords, bins=30)
+    ax2.hist2d(sim_coords, translat_sim_coords, bins=20)
     plt.xlabel(f"{labels[i]}(t) (simulation)")
-    fig.suptitle(f"Time correlation for the {labels[i]} coordinate, T={T}")
+    fig.suptitle(f"Joint distribution for the {labels[i]} coordinate, T={T}")
 
   plt.show()
 
@@ -176,29 +179,51 @@ def dtws(gt_traj, sim_traj):
   plt.show()
 
 
+
 if __name__ == '__main__':
     print("Loading... ")
     # y=np.loadtxt("y_0.01_smoothl1.dat")
-    # y=np.loadtxt("y_0.01_smoothl1.dat")
-    y = np.loadtxt('y_0.01_jac.dat')
+    y=np.loadtxt("y_0.01_smoothl1.dat")
+    # y = np.loadtxt('y_0.01_jac.dat')
     # traj=np.loadtxt("traj_0.01_smoothl1.dat")
     traj=np.loadtxt("traj_0.01_smoothl1.dat")
     print("DONE")
 
-    plot_traj(traj,y)
+    # plot_traj(traj[:5000],y[:5000])
     
     # print("Drawing histograms..")
     # draw_histogram(traj,y)
     # print("Done")
+
+    joint_distrib(traj, y, T=500)
     
-    # print("Drawing time correlation distribution..")
-    # time_correlations(traj, y, 100) # T is chosen at random here, other values should be tested
-    # print("Done")
+    '''print("Drawing time correlation distribution..")
+    time_correlations(traj, y, np.arange(10,10000,50)) # T is chosen at random here, other values should be tested
+    print("Done")'''
+
+    """# Using model jacobian to compare equilibrium and Lyapunov exponent
+    Niter = 400000
+    delta_t = 1e-2
+
+    ROSSLER_model = Rossler_model(delta_t)
+    ROSSLER_map = RosslerMap(delta_t=delta_t)
+
+    INIT = np.array([-5.75, -1.6,  0.02])
+    fix_point = newton(ROSSLER_map.v_eq,ROSSLER_map.jacobian,INIT)
+    jac_at_eq = ROSSLER_model.jacobian(torch.tensor(fix_point).float())
+    J = jac_at_eq.copy()
+    jac_at_eq[0,0] = 0
+    constant = np.array([0,0,ROSSLER_map.b])
+    print("Gradient at equilibrium state :", (jac_at_eq-np.eye(3)) @ INIT + constant)
+    
+    lyap_gt = lyapunov_exponent(traj, ROSSLER_map.jacobian, max_it=Niter, delta_t=delta_t)
+    lyap_sim = lyapunov_exponent(y, ROSSLER_model.jacobian, max_it=Niter, delta_t=delta_t)
+    #empiric_lyap = empiric_lyapounov(traj, max_it=Niter, delta_t=delta_t)
+    print("True Lyapunov Exponents :", lyap_gt, "with delta t =", delta_t)
+    print("Simulation Lyapunov Exponents :", lyap_sim, "with delta t =", delta_t)"""
 
     # # Right thing to do: compare two generated trajectories (instead of a simulation versus the ground truth)
     # print("Computing Lyapounov")
-    # Niter = 400000
-    # delta_t = 1e-2
     # ROSSLER_MAP = RosslerMap(delta_t=delta_t)
     # lyap = lyapunov_exponent(traj, ROSSLER_MAP.jacobian, max_it=Niter, delta_t=delta_t)[0]
     # print(f'Largest lyapounov coeffiscient of the physical system: {lyap}')
@@ -207,14 +232,14 @@ if __name__ == '__main__':
     # compare_lyapounov(traj[10:10000], lyap, y[10:10000], delta_t=delta_t) # We have to focus on the start of the trajectories to avoid overfloat
     # print("Done")
 
-    # print(('Computing FFT'))
-    # plot_fourier(traj, y)
-    # print('Done')
+    '''print(('Computing FFT'))
+    plot_fourier(traj[:4000], y[:4000])
+    print('Done')'''
 
-    print("Computing DTW...")
+    '''print("Computing DTW...")
     dtws(traj[:10000],y[:10000]) #ran on 10000 first steps
     print('Done')
-
+'''
 #stats intéressantes : max distances entre deux points (au même instant, i.e. erreur de prediction)
 #                     - max min distances, les points des deux surfaces les plus éloignés
 #                     - histogrames

time_series.py
@@ -52,18 +52,19 @@ class Rossler_model:
         return list_trajectory
 
     def jacobian(self, input_):
+        input_ = torch.tensor(input_).float()
         input_.requires_grad = True
         output, speed = self.rosler_nn.forward(input_)
 
-        jacobian = np.zeros((3,3))
+        J = np.zeros((3,3))
 
         for i in range(3): # iterate over each output dimension
             dim_score = output[0][i]
             dim_score.backward(retain_graph=True)
             gradients = input_.grad
-            jacobian[i] = gradients.data
+            J[i] = gradients.data
 
-        return jacobian
+        return (J-np.eye(3))/self.delta_t
 
 
     def save_traj(self,y,file):